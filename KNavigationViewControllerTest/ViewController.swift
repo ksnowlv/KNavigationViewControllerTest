f7158bd2b1999aa934dfc110ac045d41183fab05
ksnowlv <ksnowlv@163.com>
2017-12-12 15:54:58 +0800

    Initial Commit

---
+++ KNavigationViewControllerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KNavigationViewControllerTest
+//
+//  Created by ksnowlv on 2017/12/12.
+//  Copyright © 2017年 ksnowlv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
